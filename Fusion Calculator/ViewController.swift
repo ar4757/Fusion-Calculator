b678ae1151de6efef4901eceb8211b5ba10515ff
Andrew Ratz <ar4757@gmail.com>
2018-03-23 14:01:42 -0400

    Initial Commit

---
+++ Fusion Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fusion Calculator
+//
+//  Created by Andrew Ratz on 3/23/18.
+//  Copyright © 2018 Andrew Ratz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
