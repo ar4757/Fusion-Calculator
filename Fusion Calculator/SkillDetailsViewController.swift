768b1a2b4de6668cdf4a5826f6c13655adcae93e
ar4757 <ar4757@gmail.com>
2018-12-09 00:36:19 -0500

    Search Bar
    Search bar has been implement on the main page. Fusion UI has adjustments to section header sizes and color (red -> gray)

--- Fusion Calculator/SkillDetailsViewController.swift
+++ Fusion Calculator/SkillDetailsViewController.swift
@@ -17,6 +17,12 @@ class SkillDetailsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        costLabel.frame = CGRect(x: costLabel.frame.minX, y: costLabel.frame.minY, width: 54, height: 14)
+        costLabel.layer.cornerRadius = 5
+        costLabel.layer.masksToBounds = true
+        imageView.frame = CGRect(x: imageView.frame.minX, y: imageView.frame.minY, width: 54, height: 14)
+        imageView.layer.cornerRadius = 5
+        imageView.layer.masksToBounds = true
     }
     
 }
--- Fusion Calculator/TableViewController.swift
+++ Fusion Calculator/TableViewController.swift
@@ -27,8 +27,35 @@ struct Persona: Decodable {
 
 var myStructDictionary: [String: Persona] = [:]
 
-class TableViewController: UITableViewController {
+class TableViewController: UITableViewController, UISearchResultsUpdating {
     
+    func updateSearchResults(for searchController: UISearchController) {
+        //No Search
+        let searchText = searchController.searchBar.text!
+        if searchText.isEmpty {
+            sortedTable = Array(myStructDictionary)
+        }
+        //Search
+        else {
+            let tempTable = Array(myStructDictionary)
+            sortedTable.removeAll()
+            for element in tempTable {
+                if element.key.lowercased().hasPrefix(searchText.lowercased()) && !sortedTable.contains(where: {$0.key == element.key}) {
+                    sortedTable.append(element)
+                }
+                else {
+                    continue
+                }
+            }
+            for element in sortedTable {
+                print(element.key)
+            }
+        }
+        tableView.reloadData()
+    }
+    
+    let searchController = UISearchController(searchResultsController: nil)
+
     var sortedTable: [(key: String, value: Persona)] = []
     
     func sortByLevel() {
@@ -50,34 +77,39 @@ class TableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return myStructDictionary.count
+        return sortedTable.count
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
         let cell = tableView.dequeueReusableCell(withIdentifier: "myProtoCell") as! MyTableCell
-        let current = sortedTable[indexPath.row]
-        if (current.value.arcana == "Hierophant") {
-            cell.arcanaLabel.text = "Hiero."
-        }
-        else if (current.value.arcana == "Hanged Man") {
-            cell.arcanaLabel.text = "Hanged"
-        }
-        else if (current.value.arcana == "Temperance") {
-            cell.arcanaLabel.text = "Temper."
-        }
-        else if (current.value.arcana == "Judgement") {
-            cell.arcanaLabel.text = "Judge."
+        if (indexPath.row < sortedTable.count) {
+            let current = sortedTable[indexPath.row]
+            if (current.value.arcana == "Hierophant") {
+                cell.arcanaLabel.text = "Hiero."
+            }
+            else if (current.value.arcana == "Hanged Man") {
+                cell.arcanaLabel.text = "Hanged"
+            }
+            else if (current.value.arcana == "Temperance") {
+                cell.arcanaLabel.text = "Temper."
+            }
+            else if (current.value.arcana == "Judgement") {
+                cell.arcanaLabel.text = "Judge."
+            }
+            else {
+                cell.arcanaLabel.text = current.value.arcana
+            }
+            cell.nameLabel.text = current.key
+            cell.levelLabel.text = "\(current.value.level)"
+            cell.persona = current.value
+            cell.name = current.key
+            return cell
         }
         else {
-            cell.arcanaLabel.text = current.value.arcana
+            return cell
         }
-        cell.nameLabel.text = current.key
-        cell.levelLabel.text = "\(current.value.level)"
-        cell.persona = current.value
-        cell.name = current.key
-        return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -99,7 +131,17 @@ class TableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-                
+        
+        searchController.searchBar.text = ""
+        searchController.searchResultsUpdater = self
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.searchBarStyle = UISearchBarStyle.minimal
+        searchController.searchBar.tintColor = UIColor.red
+        let textFieldInsideUISearchBar = searchController.searchBar.value(forKey: "searchField") as? UITextField
+        textFieldInsideUISearchBar?.textColor = UIColor.white
+        tableView.tableHeaderView = searchController.searchBar
+        
         let path = Bundle.main.path(forResource: "personas", ofType: "json")
         let url = URL(fileURLWithPath: path!)
         
@@ -123,6 +165,11 @@ class TableViewController: UITableViewController {
         
         sortedTable = Array(myStructDictionary)
         
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        searchController.dismiss(animated: false, completion: nil)
     }
     
 }
